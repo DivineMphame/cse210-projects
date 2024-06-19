f9c7b0ad3784adceb0de7490b2deb9ccab677d4f
DivineMphame <mphametadiwanashe@gmail.com>
2024-06-19 11:04:24 +0200

    Added code and other files to support my scripture memorizer

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,32 @@
+
+        
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Reference scriptureReference = new Reference("Psalms", "3", "7-8");
+        Scripture scripture = new Scripture(scriptureReference, "Arise,O Lord; save me, O my God: for thou hast smitten all mine enemies upon the cheek bone;thou hast broken the teeth of the ungodly.Salvation belongeth unto the Lord:thy blessing is upon thy people.Selah.");
+        ScriptureMemorizer scriptureMemorizer = new ScriptureMemorizer(scripture);
+        
+
+        string userInput = "";
+
+        while (userInput != "quit" && scriptureMemorizer.hasWordsLeft() == true)
+        {
+            Console.Clear();
+            Console.WriteLine(string.Format("{0} {1}", scriptureReference.toString(), scriptureMemorizer.toString()));
+            Console.WriteLine();
+            Console.WriteLine("Welcome to Scripture Memorizer Game! Press enter to continue ,type 'quit' to finish.");
+            userInput = Console.ReadLine();
+            scriptureMemorizer.removeWordsFromText();
+        }
+        
+        Console.Clear();
+        Console.WriteLine(string.Format("{0} {1}", scriptureReference.toString(), scriptureMemorizer.toString()));
+        Console.WriteLine();
+        Console.WriteLine("Great job!");
     }
 }
+    
